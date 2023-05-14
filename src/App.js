bbe69e76637acc9c666e824482683e23e14eb35f
Smith Ringtho <sringtho@gmail.com>
2023-05-14 17:50:44 +0300

    Added UI design

--- src/App.js
+++ src/App.js
@@ -2,8 +2,43 @@ import React from 'react'
 
 function App () {
   return (
-    <div className="App">
-      <h1>Smith Ringtho</h1>
+    <div className="app">
+      <h1>25 + 5 clock</h1>
+      <div className='length-container'>
+        <div className='break-length'>
+          <h3>Break Length</h3>
+          <div className='break-buttons'>
+            <i className='fa fa-arrow-down'></i>
+            <div>5</div>
+            <i className='fa fa-arrow-up'></i>
+          </div>
+        </div>
+        <div className='session-length'>
+          <h3>Session Length</h3>
+          <div className='session-buttons'>
+            <i className='fa fa-arrow-down'></i>
+            <div>25</div>
+            <i className='fa fa-arrow-up'></i>
+          </div>
+        </div>
+      </div>
+      <div className='timer'>
+        <h4>Session</h4>
+        <p>24:59</p>
+      </div>
+      <div className='timer-control'>
+        <div className='pause-play'>
+          <i className='fa fa-play'></i>
+          <i className='fa fa-pause'></i>
+        </div>
+        <div className='refresh'>
+          <i className='fa fa-refresh'></i>
+        </div>
+      </div>
+      <div className='footer'>
+        <p className='coded-by'>Designed and coded by</p>
+        <p>Smith Ringtho</p>
+      </div>
     </div>
   )
 }
